f216487b291beffe3f7165a3ac2d54c822ef2675
Davi cruz <davifieledeus@gmail.com>
2024-12-30 18:38:26 -0300

    first commit

---
+++ src/main/java/tads/teste/sistemadecontrole/SistemadecontroleApplication.java
@@ -0,0 +1,13 @@
+package tads.teste.sistemadecontrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadecontroleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemadecontroleApplication.class, args);
+    }
+
+}
